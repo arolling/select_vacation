ec26b72909ae5767346a1613edba179ced58d506
Abigail Rolling <arolling@gmail.com>
2016-01-15 09:46:43 -0800

    Make functioning form that adds values to an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+  var answerArray = [];
+  var userInput;
+
+  $('form').submit(function(event) {
+    for (var i = 1; i < 6; i++) {
+      var userInput = $('select[name=question' + i + ']').val();
+      answerArray.push(userInput);
+      alert(userInput);
+      alert(answerArray);
+    }
 
 
 
+    event.preventDefault();
+  }); //end of submit function
 
+
+
+  $('.btn-refresh').click(function() {
+    window.location.reload();
+  }); // end of reload function
 }); //end of ready fx
