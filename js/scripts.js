586542987a42ae7ad74f1b87c0b1f76eba8b9421
Abigail Rolling <arolling@gmail.com>
2016-01-15 14:05:25 -0800

    Create functioning decision tree including failure mode

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,100 @@ $(document).ready(function() {
     } //end loop that generates array
 
     // alert(userInput);
-    alert(answerArray);
+    // alert(answerArray);
 
     // branching decision tree starts here
 
+    if (answerArray[0] === "1a" && answerArray[1] === "2a" && answerArray[2] === "3b" && answerArray[3] === "4a" && answerArray[4] === "5b") {
+      $("#haiti").show();
+    } else if (answerArray[0] === "1c" && answerArray[1] === "2c" && answerArray[2] === "3b" && answerArray[3] === "4c" && answerArray[4] === "5b") {
+      $("#australia").show();
+    } else if (answerArray[0] === "1b" && answerArray[1] === "2b" && answerArray[2] === "3c" && answerArray[3] === "4b" && answerArray[4] === "5c") {
+      $("#milan").show();
+    } else if (answerArray[0] === "1a" && answerArray[1] === "2b" && answerArray[2] === "3a" && answerArray[3] === "4b" && answerArray[4] === "5a") {
+      $("#montreal").show();
+    } else if (answerArray[0] === "1d" && answerArray[1] === "2c" && answerArray[2] === "3c" && answerArray[3] === "4b" && answerArray[4] === "5c") {
+      $("#beijing").show();
+    } else if (answerArray[0] === "1c" && answerArray[1] === "2d" && answerArray[2] === "3c" && answerArray[3] === "4c" && answerArray[4] === "5b") {
+      $("#cairo").show();
+    } else {
+      $("#failure").show();
+    }
 
     //branching decision tree ends here
 
+    //THE BELOW IS BAD AND I SHOULD FEEL BAD
+    // if (answerArray[0] === "1a") {
+    //   $("#haiti").show();
+    //   $("#montreal").show();
+    // } else if (answerArray[0] === "1b") {
+    //   $("#milan").show();
+    // } else if (answerArray[0] === "1c") {
+    //   $("#australia").show();
+    // } else if (answerArray[0] === "1d") {
+    //   if (answerArray[1] === "2a") {
+    //     $("#haiti").show();
+    //   } else if (answerArray[1] === "2b") {
+    //     if (answerArray[2] === "3a") {
+    //       if (answerArray[3] === "4a") {
+    //         if (answerArray[4] === "5a") {
+    //
+    //         } else if (answerArray[4] === "5b") {
+    //
+    //         } else if (answerArray[4] === "5c") {
+    //
+    //         } else {
+    //           alert("Sorry, no destinations found");
+    //         }
+    //       }
+    //     } else if (answerArray[2] === "3b") {
+    //       if (answerArray[3] === "4a") {
+    //         if (answerArray[4] === "5a") {
+    //
+    //         } else if (answerArray[4] === "5b") {
+    //
+    //         } else if (answerArray[4] === "5c") {
+    //
+    //         } else {
+    //           alert("Sorry, no destinations found");
+    //         }
+    //       }
+    //
+    //     } else if (answerArray[2] === "3c") {
+    //       if (answerArray[3] === "4a") {
+    //         if (answerArray[4] === "5a") {
+    //
+    //         } else if (answerArray[4] === "5b") {
+    //
+    //         } else if (answerArray[4] === "5c") {
+    //
+    //         } else {
+    //           alert("Sorry, no destinations found");
+    //         }
+    //       }
+    //
+    //     } else {
+    //       alert("Sorry, no destinations found");
+    //     }
+    //
+    //   } else if (answerArray[1] === "2c") {
+    //
+    //   } else if (answerArray[1] === "2d") {
+    //     $("#cairo").show();
+    //   }
+    //
+    //
+    // }
+
+    // $("#beijing").show();
+    // $("#cairo").show();
+    // $("#haiti").show();
+    // $("#montreal").show();
+    // $("#australia").show();
 
 
-    $("#haiti").show();
+
+    // $("#haiti, #milan").show();
 
 
 
