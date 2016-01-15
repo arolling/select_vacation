6ccddf291132470636da1f50ed3d637dc06bfae6
Abigail Rolling <arolling@gmail.com>
2016-01-15 11:37:50 -0800

    Create question content

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,20 @@ $(document).ready(function() {
     for (var i = 1; i < 6; i++) {
       var userInput = $('select[name=question' + i + ']').val();
       answerArray.push(userInput);
-      // alert(userInput);
-      // alert(answerArray);
+
     } //end loop that generates array
 
     // alert(userInput);
     alert(answerArray);
-    $(".hideme").show();
+
+    // branching decision tree starts here
+
+
+    //branching decision tree ends here
+
+
+
+    $("#haiti").show();
 
 
 
